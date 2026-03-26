8653744fcb02b167ec1927062d29f6aa6ffcfc9c
최원종 <tye4703@naver.com>
2026-03-26 10:43:57 +0900

    제네릭프로그래밍

---
+++ src/generic/ch01/ThreeDPrinter.java
@@ -0,0 +1,36 @@
+package generic.ch01;
+
+public class ThreeDPrinter {
+
+    //재료
+    Object material;
+
+
+    //재료를 꺼냄
+    private Object getMaterial() {
+        return material;
+    }
+
+    //재료를 넣음
+    public void setMaterial(Object material) {
+        this.material = material;
+    }
+
+    //테스트 코드
+    public static void main(String[] args) {
+        Plastic plastic = new Plastic();
+        Powder powder = new Powder();
+
+        ThreeDPrinter threeDPrinter = new ThreeDPrinter();
+        threeDPrinter.setMaterial(plastic);
+        System.out.println(threeDPrinter.getMaterial());
+        System.out.println("-----------------");
+        threeDPrinter.setMaterial(powder);
+        System.out.println(threeDPrinter.getMaterial());
+        System.out.println("---------------------");
+
+        //타입을 선언해서 저장 시켜보자
+        Plastic tempPlastic = (Plastic) threeDPrinter.getMaterial();
+        System.out.println("프로그램을 종료합니다");
+    }
+}
---
+++ src/generic/ch03/MainTest.java
@@ -0,0 +1,27 @@
+package generic.ch03;
+
+import generic.ch02.GenericPrinter;
+import generic.ch02.Water;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+
+        Water water1 = new Water();
+        Plastic plastic1 = new Plastic();
+        Powder powder1 = new Powder();
+
+        //GenericExtendsPrinter <-- 제네릭 클래스로 설계 되어 있어서 <> 를 사용할 수 있다.
+
+        //GenericExtendsPrinter<> printer = new GenericExtendsPrinter();
+        //오류 발생 = Water 타입은 Material의 자식이 아니기 때문에 사용할 수 없음
+
+        GenericExtendsPrinter<Plastic> printer = new GenericExtendsPrinter<>();
+        printer.setMaterial(plastic1); //주소 값
+
+        //System.out.println(printer.getMaterial().showInfo());<-- showInfo()가 void반환하기 때문에 오류
+        System.out.println(printer.getMaterial());
+        printer.getMaterial().showInfo();
+
+    }
+}
